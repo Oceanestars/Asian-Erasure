fa1d14e0d8579e5012add6d395cfee26e9d47700
dandan502 <danliu.5157@gmail.com>
2021-09-12 18:12:36 +0800

    updated navbar & logo css

--- src/App.js
+++ src/App.js
@@ -5,31 +5,28 @@ import { Home } from "./Components/Home/Home";
 import { Pitfalls } from "./Components/Pitfalls/Pitfalls";
 import { AboutUs } from "./Components/About/AboutUs";
 
-import './App.css';
+import "./App.css";
 import { colors } from "@material-ui/core";
 
 function App() {
   return (
-<Router>
-    <main>
-      <h1 style={{backgroundColor:"#46B893", color:"white", display:"inline-block"}}>Asian Erasure</h1>
-      <nav> 
-        <ul style={{display:"inline-block"}}> 
-          <ZoomNavigationMenuStyle/>
-        </ul>
-      </nav>
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/trailblazers"  component={Trailblazers} />
-      <Route path="/pitfalls"  component={Pitfalls} />
-      <Route path="/about"  component={AboutUs} />
-    </Switch>
-    </main>
-    <body>
-      
-    </body>
-</Router>
-
+    <Router>
+      <main>
+        <h1>Asian Erasure</h1>
+        <nav>
+          <ul>
+            <ZoomNavigationMenuStyle />
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/trailblazers" component={Trailblazers} />
+          <Route path="/pitfalls" component={Pitfalls} />
+          <Route path="/about" component={AboutUs} />
+        </Switch>
+      </main>
+      <body></body>
+    </Router>
   );
 }
 
