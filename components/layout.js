405537ed0fa50a220d8595715828d3b79ab3ad69
Oceane Andreis <ocan3794@colorado.edu>
2021-11-15 23:46:24 -0700

    Added Nunito font import everywhere and added title tag

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,15 @@
 import Header from './header'
 import Navbar from './navbar'
 import Footer from './footer'
+import Head from 'next/head'
 
 export default function Layout({ children }) {
   return (
     <>
+      <Head>
+        <title>Asian Erasure</title>
+        <meta property="og:title" content="Asian Erasure" key="title" />
+      </Head>
       <Header/>
       <Navbar />
       <main>{children}</main>
