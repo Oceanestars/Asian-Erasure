0e49071d0a5b2c9cb4abb9c71a85671d03767e48
oceanestars <ocan3794@colorado.edu>
2021-12-05 22:17:03 -0700

    Google font attempt

--- components/layout.js
+++ components/layout.js
@@ -9,6 +9,9 @@ export default function Layout({ children }) {
       <Head>
         <title>Asian Erasure</title>
         <meta property="og:title" content="Asian Erasure" key="title" />
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600;700;800;900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&display=swap" rel="stylesheet" />
+
       </Head>
       <Header/>
       <Navbar />
--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,9 @@ import '../styles/global.scss'
 export default function App({ Component, pageProps }) {
     return (
       <Layout>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600;700;800;900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&display=swap" rel="stylesheet" />
+
         <Component {...pageProps} />
       </Layout>
     )
